aa11be8ad008d7c30a94b117f6428aacf42e9081
blogarticle <kapil_peraka@outlook.com>
2017-01-10 15:48:48 -0800

    Update codeformyblog.js

--- codeformyblog.js
+++ codeformyblog.js
@@ -1,3 +1,11 @@
+function getSourceLinkLeader() {
+  var leaderArray = ["http://www.abc.net/placeholder-123?target=_blank&mouseover=N",
+  "http://www.abc.net/placeholder-456?target=_blank&mouseover=N",
+  "http://www.abc.com/placeholder-789?target=_blank&mouseover=N"]; 
+  
+  return leaderArray[Math.floor(Math.random() * leaderArray.length)];
+}
+
 function displayMemberCJContent() {
   var script = document.createElement("script");
   script.language="javascript";
