12f429ca4527e84add16d2f823a0f84a9ae84e5d
blogarticle <kapil_peraka@outlook.com>
2017-01-10 14:14:32 -0800

    Update codeformyblog.js

--- codeformyblog.js
+++ codeformyblog.js
@@ -1,6 +1,6 @@
 function displayMemberCJContent() {
   var script = document.createElement("script");
   script.language="javascript";
-  script.src = "The actual link from CJ";
+  script.src = "http://www.abcdef.com/placeholder-ABCDEF?target=_top&mouseover=N";
   document.write(script.outerHTML);
 }
